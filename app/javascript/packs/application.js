7e86cd6948dc443e3a6043bbaec32de70ec18ed1
Jose <jocvegar@gmail.com>
2021-04-16 21:05:18 -0600

    Adds bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,8 @@ require("channels")
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../stylesheets/application'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
